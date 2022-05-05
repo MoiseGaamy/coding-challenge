e8dfec2a30625c0927b809d33beff1f8198b3cba
Gamy <flokygamy@gmail.com>
2022-05-05 06:32:09 +0000

    sample to set things

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+   <div style={{ backgroundColor: "#006E7F" }}>
+      <div>ji</div>
+      <div>tek</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+    </div>
   )
+  
 }
