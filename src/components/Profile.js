8efaaf66804cb897868732c7dfa7c33be2a4534f
Gamy <flokygamy@gmail.com>
2022-05-05 06:33:34 +0000

    testing profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div style={{ backgroundColor: "#68A7AD" }}>
+      <div>
+        <img src="" alt='profile' className="img-style"/>
+      </div>
+      <div>tek</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+      <div>pickak</div>
+    </div>
   )
 }
