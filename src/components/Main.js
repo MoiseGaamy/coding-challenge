e85e7a812d2b522b4fb4dc392a0d543541fbbd39
Gamy <flokygamy@gmail.com>
2022-05-06 08:42:19 +0000

    changed col

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,10 +6,10 @@ export default function Main() {
   return (
     <div className="fluid-container main-container">
         <div className="row">
-          <div className="col-lg-3 col-md-4 col-sm-6 custom-profile">
+          <div className="col-lg-3 col-md-8 col-sm-6 custom-profile">
               <Profile />
           </div>  
-          <div className="col-lg-8 col-md-8 col-sm-6 custom-dashboard">
+          <div className="col-lg-8 col-md-11 col-sm-6 custom-dashboard">
               <Dashboard />
           </div>  
         </div>
