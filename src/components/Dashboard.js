a237655e71d68fb7af5781436c45103f2ad63071
Gamy <flokygamy@gmail.com>
2022-05-06 05:10:30 +0000

    implemented dahsboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import '../css/dashboard.css'
+import DashSale from './DashSale.js'
+import DashProfit from './DashProfit.js'
+import DashClient from './DashClient.js'
 
 export default function Dashboard() {
   return (
-   <div style={{ backgroundColor: "#006E7F" }}>
-      <div>ji</div>
-      <div>tek</div>
-      <div>pickak</div>
-      <div>pickak</div>
-      <div>pickak</div>
-      <div>pickak</div>
-      <div>pickak</div>
+   <div className="dashboard-container">
+      <div className="fluid-container dashboard-custom-container">
+       <DashSale />
+       <DashProfit />
+       <DashClient />
+      </div>
+      
     </div>
   )
   
