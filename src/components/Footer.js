2f68ebf232bd3af044086de8321689c87102ba67
Gamy <flokygamy@gmail.com>
2022-05-06 08:41:44 +0000

    footer responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,25 +6,25 @@ import '../css/footer.css'
 
 export default function Footer() {
   return (
-   <div className="container">
-       <div className="row">
-           <div className="col-lg-3 col-md-4 col-sm-6"></div>
-           <div className="col-lg-3 col-md-4 col-sm-6">
+   <div className="container footer-container">
+       <div className="row sm-row">
+           <div className="col-lg-3 col-md-3 col-sm-6"></div>
+           <div className="col-lg-3 col-md-3 col-sm-6">
                 <h3 className="custom-heading">About</h3>
            </div>
-           <div className="col-lg-3 col-md-4 col-sm-6">
+           <div className="col-lg-3 col-md-3 col-sm-6">
                 <h3 className="custom-heading">Support</h3>
            </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="col-lg-3 col-md-3 col-sm-6">
                 <h3 className="custom-heading">Address</h3>
            </div>
        </div>
-       <div className="row">
-           <div className="col-lg-3 col-md-4 col-sm-6">
+       <div className="row sm-row2">
+           <div className="col-lg-3 col-md-3 col-sm-6">
                <p>Counter Delivery,Carters Beach</p>
                <p>PostCentre,Westport</p>
            </div>
-           <div className="col-lg-3 col-md-4 col-sm-6">
+           <div className="col-lg-3 col-md-3 col-sm-6">
                  <ul className="d-flex flex-column footer-link bd-highlight">
                       <a href="#">Company</a>
                       <a href="#">Team</a>
@@ -32,7 +32,7 @@ export default function Footer() {
                       <a href="#">Blog</a>
                </ul>
            </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="col-lg-3 col-md-3 col-sm-6">
                    <ul className="d-flex flex-column footer-link bd-highlight">
                       <a href="#">Help Center</a>
                       <a href="#">Cancellation Options</a>
@@ -40,7 +40,7 @@ export default function Footer() {
                       <a href="#">Trust & Safety</a>
                </ul>
            </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
+              <div className="col-lg-3 col-md-3 col-sm-6">
                    <ul className="d-flex flex-column footer-link bd-highlight">
                       <a href="#">Counter Delivery,Carters Beach</a>
                       <a href="#">PostCentre,Westport</a>
@@ -58,7 +58,7 @@ export default function Footer() {
                   </div>
            </div>
        </div>
-       <div className="row">
+       <div className="row sm-row3 ">
          <div className="col-lg-4 col-md-4 col-sm-4">
              <p className="text-right">CUdwell,LLC.All rights reserved.</p>
          </div>
