a184388a041f5d5779c233c64d9efc6672440c1b
Gamy <flokygamy@gmail.com>
2022-05-06 05:14:34 +0000

    implemented main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,9 @@ import Dashboard from './Dashboard.js'
 
 export default function Main() {
   return (
-    <div className="container main-container">
+    <div className="fluid-container main-container">
         <div className="row">
-          <div className="col-lg-3 col-md-4 col-sm-6 offset-2 custom-profile">
+          <div className="col-lg-3 col-md-4 col-sm-6 custom-profile">
               <Profile />
           </div>  
           <div className="col-lg-8 col-md-8 col-sm-6 custom-dashboard">
